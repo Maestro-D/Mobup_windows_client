5168786b8a56bb0ed95bbb235add556861aaaddf
Mehdi Boutet-Amami <system2lost@hotmail.fr>
2018-05-12 22:49:22 +0200

    Tentative d'implementer juste une view, et pas moyen au bout de 2h, je pete un plomb

--- EIP_Client_Windows/src/eip_client_windows/FeatureController.java
+++ EIP_Client_Windows/src/eip_client_windows/FeatureController.java
@@ -52,6 +52,7 @@ public class FeatureController implements Initializable {
     public Button left, right;
     public GridPane gridpane;
     public VBox box;
+    public Label item_label;
     
     
     @FXML
@@ -65,11 +66,23 @@ public class FeatureController implements Initializable {
             } catch (IOException ex) {
                 Logger.getLogger(WelcomeController.class.getName()).log(Level.SEVERE, null, ex);
             }
-        
+    }
+    
+    @FXML
+    public void Go_Back(ActionEvent event){
+            try {
+                root = FXMLLoader.load(getClass().getResource("Inventory.fxml"));
+                scene = new Scene(root);
+                mainstage = (Stage)  ((Node)event.getSource()).getScene().getWindow();
+                mainstage.setScene(scene);
+                mainstage.show();
+            } catch (IOException ex) {
+                Logger.getLogger(FeatureController.class.getName()).log(Level.SEVERE, null, ex);
+            }
     }
     
         private void viewItem(String id){
-            
+            System.out.println("Je suis dans ViewItem pour l'item" + id);
         }
 
         private void Inventory(){
@@ -77,18 +90,32 @@ public class FeatureController implements Initializable {
             String a[] = {"Potion", "Epee", "Bouclier", "Cube"}; // remplacer par liste d'objets reçus
             int item_size = a.length; // remplacer par la taille de la liste d'objets reçus
             
-            for (int i = 0; i < item_size; i++)
+            for (int i = 0; i < item_size ; i++)
                 {
-                    System.out.println(a[i]);
+                    System.out.println(a[i] + " et item_size vaut " + item_size);
                     Button item = new Button(a[i]);
                     item.setId(Integer.toString(i)); //remplacer paramètre par id de l'item en cours
-                    item.setOnAction(new EventHandler<ActionEvent>() {
-                     @Override public void handle(ActionEvent e) {
-                        viewItem(item.getId());
+                    item.setOnAction((ActionEvent e) -> {
+                        System.out.println("Je suis dans mon eventhandler ! " + item.getId());
+                        try {
+                            System.out.println("EventHandler: etape 1");
+                            root = FXMLLoader.load(getClass().getResource("Item.fxml"));
+                            System.out.println("EventHandler: etape 2");
+                            scene = new Scene(root);
+                            System.out.println("EventHandler: etape 3");
+                            mainstage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+                            System.out.println("EventHandler: etape 4");
+                            mainstage.setScene(scene);
+                            mainstage.show();
+                            System.out.println("EventHandler: etape 5");
+                        } catch (IOException ex) {
+                            Logger.getLogger(FeatureController.class.getName()).log(Level.SEVERE, null, ex);
                         }
+                        viewItem(item.getId());
                     });
                     box.getChildren().add(item);
                 }
+            System.out.println("for fini !");
         }
     
         @Override
