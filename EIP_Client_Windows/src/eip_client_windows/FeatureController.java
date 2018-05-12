af77bf173b407a40c4423df66c90f2f4ec708399
Mehdi Boutet-Amami <system2lost@hotmail.fr>
2018-05-12 21:33:45 +0200

    Tentative n°64684135168465168486 de PUTAIN de commit

--- EIP_Client_Windows/src/eip_client_windows/FeatureController.java
+++ EIP_Client_Windows/src/eip_client_windows/FeatureController.java
@@ -10,15 +10,31 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.layout.VBox;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Popup;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
-
+import javafx.stage.Window;
 /**
  * FXML Controller class
  *
@@ -28,10 +44,15 @@ public class FeatureController implements Initializable {
 
     Parent root;
     Scene scene;
-    private Stage mainstage;
-    /**
-     * Initializes the controller class.
-     */
+    Stage mainstage;  
+    Window test;
+    
+    @FXML
+    private ScrollPane scrollPane;
+    public Button left, right;
+    public GridPane gridpane;
+    public VBox box;
+    
     
     @FXML
     public void Go_Home(ActionEvent event){
@@ -47,11 +68,31 @@ public class FeatureController implements Initializable {
         
     }
     
+        private void viewItem(String id){
+            
+        }
+
+        private void Inventory(){
+            Popup popup = new Popup();
+            String a[] = {"Potion", "Epee", "Bouclier", "Cube"}; // remplacer par liste d'objets reçus
+            int item_size = a.length; // remplacer par la taille de la liste d'objets reçus
+            
+            for (int i = 0; i < item_size; i++)
+                {
+                    System.out.println(a[i]);
+                    Button item = new Button(a[i]);
+                    item.setId(Integer.toString(i)); //remplacer paramètre par id de l'item en cours
+                    item.setOnAction(new EventHandler<ActionEvent>() {
+                     @Override public void handle(ActionEvent e) {
+                        viewItem(item.getId());
+                        }
+                    });
+                    box.getChildren().add(item);
+                }
+        }
     
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        @Override
+        public void initialize(URL url, ResourceBundle rb) {
+            this.Inventory();
     }    
-    
 }
